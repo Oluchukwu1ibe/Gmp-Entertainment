a735c240889ecdb46fbc563d5e5fd57324c32e9d
olukinni_029 <olukinnif`150216029@gmail.com>
2024-04-30 13:27:04 +0100

    Added a new Package

--- User/src/controller/authController.js
+++ User/src/controller/authController.js
@@ -3,6 +3,8 @@ import {
   createJwtToken,
   verifyJwtToken,
 } from "../middleware/token.js";
+import _ from "lodash";
+import bcrypt from "bcrypt";
 import nodemailer from "nodemailer";
 
 export const register = async (req, res) => {
