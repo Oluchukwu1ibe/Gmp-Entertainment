8f29f6519005c060c6a0c9262af9661af8972d49
olukinni_029 <olukinnif`150216029@gmail.com>
2024-10-12 11:07:05 +0100

    Fixing validation errors

--- User/src/utils/validation.js
+++ User/src/utils/validation.js
@@ -4,12 +4,13 @@ const updateContestantSchema = Joi.object({
     displayName: Joi.string().required().min(1).max(50),
     location: Joi.string().required().min(1).max(100),
     dob: Joi.string().required().custom((value, helpers) => {
-        const datePattern = /^\d{1,2},\d{1,2},\d{4}$/;
-        if (!datePattern.test(value)) {
-          return helpers.message('Dob must be in M-D-YYYY format (e.g., 3-2-1990)');
-        }
-        return value;
-      }), 
+      const datePattern = /^\d{1,2}-\d{1,2}-\d{4}$/;
+      if (!datePattern.test(value)) {
+        return helpers.message('Dob must be in M-D-YYYY format (e.g., 3-2-1990)');
+      }
+      return value;
+    }),
+    
     sex: Joi.string().required().valid('Male', 'Female', 'Other'),
     occupation: Joi.string().required().min(1).max(100),
     aboutMe: Joi.string().required().min(1).max(500)
